99742ac696d76bee49c6b4606100a07e4e6d92ee
patriksinox <patrik.subjak1502@gmail.com>
2022-11-23 19:39:03 +0100

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("## Markdown Ukážka");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-6">
+          <textarea
+            value={text}
+            className="shadow-lg p-5"
+            onChange={(e) => setText(e.target.value)}
+          ></textarea>
+        </div>
+        <div className="col-sm-6">
+          <div className="obsah shadow-lg p-5">
+            <ReactMarkdown>{text}</ReactMarkdown>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
